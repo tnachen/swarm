646306e4501642563756d9b803b73af63336a457
Isabel Jimenez <contact.isabeljimenez@gmail.com>
2015-04-10 19:41:44 -0400

    Use mesos values for resource accounting (mem, cpus) and change some naming
    Signed-off-by: Isabel Jimenez <contact.isabeljimenez@gmail.com>

--- cluster/mesos/slave.go
+++ cluster/mesos/slave.go
@@ -6,6 +6,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/swarm/cluster"
+	"github.com/docker/swarm/scheduler/node"
 	"github.com/gogo/protobuf/proto"
 	"github.com/mesos/mesos-go/mesosproto"
 	mesosscheduler "github.com/mesos/mesos-go/scheduler"
@@ -26,11 +27,49 @@ func NewSlave(addr string, overcommitRatio float64, offer *mesosproto.Offer) *sl
 	return slave
 }
 
-func (n *slave) addOffer(offer *mesosproto.Offer) {
-	n.offers = append(n.offers, offer)
+func (s *slave) toNode() *node.Node {
+	return &node.Node{
+		ID:          s.ID,
+		IP:          s.IP,
+		Addr:        s.Addr,
+		Name:        s.Name,
+		Cpus:        s.Cpus,
+		Labels:      s.Labels,
+		Containers:  s.Containers(),
+		Images:      s.Images(),
+		UsedMemory:  s.UsedMemory(),
+		UsedCpus:    s.UsedCpus(),
+		TotalMemory: s.TotalMemory(),
+		TotalCpus:   s.TotalCpus(),
+		IsHealthy:   s.IsHealthy(),
+	}
 }
 
-func (n *slave) create(driver *mesosscheduler.MesosSchedulerDriver, config *dockerclient.ContainerConfig, name string, pullImage bool) (*cluster.Container, error) {
+func (s *slave) addOffer(offer *mesosproto.Offer) {
+	s.offers = append(s.offers, offer)
+}
+
+func (s *slave) scalarResourceValue(name string) float64 {
+	var value float64
+	for _, offer := range s.offers {
+		for _, resource := range offer.Resources {
+			if *resource.Name == name {
+				value += *resource.Scalar.Value
+			}
+		}
+	}
+	return value
+}
+
+func (s *slave) UsedMemory() int64 {
+	return s.TotalMemory() - int64(s.scalarResourceValue("mem"))*1024*1024
+}
+
+func (s *slave) UsedCpus() int64 {
+	return s.TotalCpus() - int64(s.scalarResourceValue("cpus"))
+}
+
+func (s *slave) create(driver *mesosscheduler.MesosSchedulerDriver, config *dockerclient.ContainerConfig, name string, pullImage bool) (*cluster.Container, error) {
 
 	id := make([]byte, 6)
 	nn, err := rand.Read(id)
@@ -39,7 +78,7 @@ func (n *slave) create(driver *mesosscheduler.MesosSchedulerDriver, config *dock
 	}
 	ID := hex.EncodeToString(id)
 
-	n.updates[ID] = make(chan string)
+	s.updates[ID] = make(chan string)
 
 	cpus := "cpus"
 	typ := mesosproto.Value_SCALAR
@@ -50,7 +89,7 @@ func (n *slave) create(driver *mesosscheduler.MesosSchedulerDriver, config *dock
 		TaskId: &mesosproto.TaskID{
 			Value: &ID,
 		},
-		SlaveId: n.offers[0].SlaveId,
+		SlaveId: s.offers[0].SlaveId,
 		Resources: []*mesosproto.Resource{
 			{
 				Name: &cpus,
@@ -82,7 +121,7 @@ func (n *slave) create(driver *mesosscheduler.MesosSchedulerDriver, config *dock
 
 	offerIds := []*mesosproto.OfferID{}
 
-	for _, offer := range n.offers {
+	for _, offer := range s.offers {
 		offerIds = append(offerIds, offer.Id)
 	}
 
@@ -92,21 +131,21 @@ func (n *slave) create(driver *mesosscheduler.MesosSchedulerDriver, config *dock
 	}
 	log.Debugf("create %v: %v", status, err)
 
-	n.offers = []*mesosproto.Offer{}
+	s.offers = []*mesosproto.Offer{}
 
 	// block until we get the container
-	<-n.updates[ID]
+	<-s.updates[ID]
 
 	// Register the container immediately while waiting for a state refresh.
 	// Force a state refresh to pick up the newly created container.
-	n.RefreshContainers(true)
+	s.RefreshContainers(true)
 
-	n.RLock()
-	defer n.RUnlock()
+	s.RLock()
+	defer s.RUnlock()
 
 	// TODO: We have to return the right container that was just created.
 	// Once we receive the ContainerID from the executor.
-	for _, container := range n.Containers() {
+	for _, container := range s.Containers() {
 		return container, nil
 	}
 
